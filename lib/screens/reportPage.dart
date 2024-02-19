a5923f3d717864e7ad4ce0d144d1fe4cda3d0e59
uumair327 <mohdumair.a@somaiya.edu>
2024-02-20 00:49:26 +0530

    Report Page Updated

--- lib/screens/reportPage.dart
+++ lib/screens/reportPage.dart
@@ -1,160 +1,75 @@
 import 'package:flutter/material.dart';
 
-import 'CaseQuestionsPage.dart'; // Import the CaseQuestionsPage
-
 class ReportPage extends StatelessWidget {
-  const ReportPage({Key? key});
-
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Report an Issue'),
-        ),
-        body: SingleChildScrollView(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              _buildReportTitle(),
-              SizedBox(height: 20),
-              _buildCategoryButtons(context),
-            ],
-          ),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Report Incident'),
       ),
-    );
-  }
-
-  Widget _buildReportTitle() {
-    return Text(
-      'Report an Issue',
-      style: TextStyle(
-        fontSize: 24,
-        fontWeight: FontWeight.bold,
-      ),
-    );
-  }
-
-  Widget _buildCategoryButtons(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        _buildCardButton(
-            context, 'Environmental Safety', _environmentalSafetySubTopics),
-        SizedBox(height: 10),
-        _buildCardButton(context, 'Online Safety', _onlineSafetySubTopics),
-        SizedBox(height: 10),
-        _buildCardButton(
-            context, 'Educational Safety', _educationalSafetySubTopics),
-        SizedBox(height: 10),
-        _buildCardButton(context, 'Mental Health', _mentalHealthSubTopics),
-        SizedBox(height: 10),
-        _buildCardButton(
-            context, 'Community Safety', _communitySafetySubTopics),
-        SizedBox(height: 10),
-        _buildCardButton(context, 'Promoting Positive Development',
-            _positiveDevelopmentSubTopics),
-      ],
-    );
-  }
-
-  Widget _buildCardButton(
-      BuildContext context, String category, List<String> subTopics) {
-    return Card(
-      elevation: 4,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: InkWell(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => CaseQuestionsPage(category, subTopics),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Select Incident Type:',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          );
-        },
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                category,
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
+            SizedBox(height: 8),
+            DropdownButtonFormField<String>(
+              items: [
+                DropdownMenuItem(
+                  child: Text('Accident'),
+                  value: 'accident',
                 ),
+                DropdownMenuItem(
+                  child: Text('Theft'),
+                  value: 'theft',
+                ),
+                DropdownMenuItem(
+                  child: Text('Fire'),
+                  value: 'fire',
+                ),
+              ],
+              onChanged: (value) {
+                // Implement logic based on selected incident type
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Select Incident Type',
               ),
-              SizedBox(height: 10),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: subTopics
-                    .map(
-                      (subTopic) => ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  CaseQuestionsPage(category, [subTopic]),
-                            ),
-                          );
-                        },
-                        child: Text(subTopic),
-                      ),
-                    )
-                    .toList(),
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Location',
+                border: OutlineInputBorder(),
               ),
-            ],
-          ),
+              keyboardType: TextInputType.text,
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Description',
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Implement submit button logic
+              },
+              child: Text('Submit'),
+            ),
+          ],
         ),
       ),
     );
   }
-
-  // Subtopics for each category
-  static const List<String> _environmentalSafetySubTopics = [
-    'Poisoning',
-    'Fire Safety',
-    'Drowning',
-    'Falls',
-    'Exposure to dangerous chemicals or toxins',
-  ];
-
-  static const List<String> _onlineSafetySubTopics = [
-    'Cyberbullying',
-    'Exposure to inappropriate content',
-    'Online predators',
-    'Addiction to gaming or social media',
-  ];
-
-  static const List<String> _educationalSafetySubTopics = [
-    'Physical violence or bullying',
-    'Emotional abuse or neglect',
-    'Exposure to hazardous materials or unsafe environments',
-  ];
-
-  static const List<String> _mentalHealthSubTopics = [
-    'Anxiety and depression',
-    'Eating disorders',
-    'Self-harm or suicidal thoughts',
-    'The impact of trauma or abuse',
-  ];
-
-  static const List<String> _communitySafetySubTopics = [
-    'Gang violence',
-    'Drug use and abuse',
-    'Human trafficking',
-    'Exposure to violence or crime',
-  ];
-
-  static const List<String> _positiveDevelopmentSubTopics = [
-    'Providing access to quality education and healthcare',
-    'Creating safe and nurturing environments',
-    'Teaching children about safety and risk prevention',
-    'Building strong and supportive relationships with children',
-  ];
 }
