e7e6718922383076eec874170948f3f3250914c3
uumair327 <mohdumair.a@somaiya.edu>
2024-03-14 13:13:10 +0530

    Category wise video Added

--- lib/screens/learn.dart
+++ lib/screens/learn.dart
@@ -10,58 +10,93 @@ class VideoPage extends StatefulWidget {
 
 class _VideoPageState extends State<VideoPage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  late List<String> categories;
+  String? selectedCategory;
 
   @override
   void initState() {
     super.initState();
-    // Check if the user is authenticated
-    User? user = _auth.currentUser;
-    if (user == null) {
-      // Handle unauthenticated state
-    } else {
-      // User is authenticated
-    }
+    fetchCategories();
+  }
+
+  Future<void> fetchCategories() async {
+    QuerySnapshot querySnapshot =
+        await FirebaseFirestore.instance.collection('videos').get();
+    Set<String> categorySet = Set();
+    querySnapshot.docs.forEach((doc) {
+      final category = doc['category'] as String?;
+      if (category != null) {
+        categorySet.add(category);
+      }
+    });
+    setState(() {
+      categories = categorySet.toList();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Video List'),
+        title: Text(selectedCategory ?? 'Video List'),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('videos').snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          }
-          // Extract videos from the snapshot
-          List<QueryDocumentSnapshot> videos = snapshot.data!.docs;
-          return ListView.builder(
-            itemCount: videos.length,
-            itemBuilder: (context, index) {
-              var video = videos[index].data() as Map<String, dynamic>;
-              return ListTile(
-                title: Text(video['title'] ?? 'Untitled'),
-                onTap: () {
-                  // Navigate to video player page passing the video URL
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => VideoPlayerPage(
-                        videoUrl: video['videoUrl'] as String? ?? '',
-                      ),
+      body: selectedCategory != null
+          ? buildVideoList(selectedCategory!)
+          : buildCategoryList(),
+    );
+  }
+
+  Widget buildCategoryList() {
+    return ListView.builder(
+      itemCount: categories.length,
+      itemBuilder: (context, index) {
+        final category = categories[index];
+        return ListTile(
+          title: Text(category),
+          onTap: () {
+            setState(() {
+              selectedCategory = category;
+            });
+          },
+        );
+      },
+    );
+  }
+
+  Widget buildVideoList(String category) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance
+          .collection('videos')
+          .where('category', isEqualTo: category)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        }
+        List<QueryDocumentSnapshot> videos = snapshot.data!.docs;
+        return ListView.builder(
+          itemCount: videos.length,
+          itemBuilder: (context, index) {
+            var video = videos[index].data() as Map<String, dynamic>;
+            return ListTile(
+              title: Text(video['title'] ?? 'Untitled'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => VideoPlayerPage(
+                      videoUrl: video['videoUrl'] as String? ?? '',
                     ),
-                  );
-                },
-              );
-            },
-          );
-        },
-      ),
+                  ),
+                );
+              },
+            );
+          },
+        );
+      },
     );
   }
 }
@@ -73,23 +108,18 @@ class VideoPlayerPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Extract video ID from the YouTube URL
     String? videoId = YoutubePlayer.convertUrlToId(videoUrl);
-
-    // Check if the video ID is null
     if (videoId == null) {
-      // Handle the case where the video ID is null
       return Scaffold(
         appBar: AppBar(
-          title: const Text('Video Player'),
+          title: Text('Video Player'),
         ),
-        body: const Center(
+        body: Center(
           child: Text('Invalid YouTube URL'),
         ),
       );
     }
 
-    // Create a YoutubePlayerController
     YoutubePlayerController _controller = YoutubePlayerController(
       initialVideoId: videoId,
       flags: const YoutubePlayerFlags(
@@ -98,26 +128,19 @@ class VideoPlayerPage extends StatelessWidget {
       ),
     );
 
-    // Implement video player here
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Video Player'),
+        title: Text('Video Player'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Center(
-              child: YoutubePlayer(
-                controller: _controller,
-                showVideoProgressIndicator: true,
-                progressIndicatorColor: Colors.blueAccent,
-                progressColors: const ProgressBarColors(
-                  playedColor: Colors.blue,
-                  handleColor: Colors.blueAccent,
-                ),
-              ),
-            ),
-          ],
+      body: Center(
+        child: YoutubePlayer(
+          controller: _controller,
+          showVideoProgressIndicator: true,
+          progressIndicatorColor: Colors.blueAccent,
+          progressColors: const ProgressBarColors(
+            playedColor: Colors.blue,
+            handleColor: Colors.blueAccent,
+          ),
         ),
       ),
     );
