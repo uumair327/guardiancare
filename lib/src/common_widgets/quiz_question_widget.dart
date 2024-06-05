779f7283338564685e3db01f41a4d763ea483367
uumair327 <mohdumair.a@somaiya.edu>
2024-06-05 23:14:54 +0530

    color theme CIF

--- lib/src/common_widgets/quiz_question_widget.dart
+++ lib/src/common_widgets/quiz_question_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:guardiancare/src/constants/colors.dart';
 
 class QuizQuestionWidget extends StatelessWidget {
   final int questionIndex;
@@ -16,7 +17,7 @@ class QuizQuestionWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
-        color: Colors.green, // Background color
+        color: Colors.white, // Background color
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -26,7 +27,7 @@ class QuizQuestionWidget extends StatelessWidget {
               style: const TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
-                color: Colors.white, // Text color
+                color: tTextPrimary, // Text color
               ),
             ),
             const SizedBox(height: 16),
@@ -34,7 +35,7 @@ class QuizQuestionWidget extends StatelessWidget {
               question['question'],
               style: const TextStyle(
                 fontSize: 20,
-                color: Colors.white, // Text color
+                color: tTextPrimary, // Text color
               ),
             ),
             const SizedBox(height: 16),
@@ -49,17 +50,20 @@ class QuizQuestionWidget extends StatelessWidget {
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(30.0),
                         side: const BorderSide(
-                            color: Colors.white), // White border
+                            color: tPrimaryColor), // White border
                       ),
                       padding: const EdgeInsets.symmetric(
                         vertical: 16.0,
                         horizontal: 24.0,
                       ),
-                      backgroundColor: Colors.green, // Green background color
+                      backgroundColor: Colors.white,
+                      foregroundColor: tTextPrimary,
+                      elevation: 0, // Green background color
                     ),
                     child: Text(
                       question['options'][index],
-                      style: const TextStyle(color: Colors.white), // Text color
+                      style:
+                          const TextStyle(color: tPrimaryColor), // Text color
                     ),
                   ),
                 ),
