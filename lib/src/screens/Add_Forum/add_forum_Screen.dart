d4aeedb07e1bfcb81b37a0e6ddec53dc81e93730
Adarsh <Adarshbunny293@gmail.com>
2024-06-06 12:45:34 +0530

    Feat : Add form validation to Add post Screen

--- lib/src/screens/Add_Forum/add_forum_Screen.dart
+++ lib/src/screens/Add_Forum/add_forum_Screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
  
 class AddForumScreen extends StatefulWidget {
   const AddForumScreen ({super.key});
@@ -7,44 +9,60 @@ class AddForumScreen extends StatefulWidget {
  
 }
 class _AddForumScreenState extends State<AddForumScreen> {
+  final  title = TextEditingController();
+  final description = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  bool loading = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add Blog'),
+        actions: [
+          IconButton(onPressed: (){},
+           icon: const Icon(Icons.done))
+        ]
       ),
-      body: ListView(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: const InputDecoration(
-                labelText: 'Title',
-                border: OutlineInputBorder(),
-              ),
+      body: Form (
+        key: _formKey,
+        child : ListView(
+        padding: const EdgeInsets.all(15),
+        children:[
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Title',
+              hintText: 'Enter the title of the Forum  post',
+              border: OutlineInputBorder(),
             ),
+            validator : (value){
+              if(value == null || value.isEmpty){
+                return 'Please enter the title';
+              }
+              return null;
+            }
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: const InputDecoration(
-                labelText: 'Content',
-                border: OutlineInputBorder(),
-              ),
-              maxLines: 10,
+          const SizedBox(height: 15),
+          TextFormField(
+            maxLines: 10,
+            decoration: const InputDecoration(
+              labelText: 'Description',
+              hintText: 'Enter the Description of the Forum  post',
+              border:OutlineInputBorder(),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: () {
-                // Add functionality to save the blog post
-              },
-              child: const Text('Save'),
-            ),
+            validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter the title';
+                  }
+                  return null;
+                }
           ),
         ],
       ),
+      ),
     );
+  }
+
+  addForum() async{
+
   }
 }
