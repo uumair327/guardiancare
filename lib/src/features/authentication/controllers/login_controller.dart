5b8daa57d4c29060f470dafb3dc839263529ca56
adarshrawat8304 <adarshrawat8304@gmail.com>
2024-07-06 03:09:10 +0000

    chore: added user name fetch

--- lib/src/features/authentication/controllers/login_controller.dart
+++ lib/src/features/authentication/controllers/login_controller.dart
@@ -1,19 +1,38 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<UserCredential?> signInWithGoogle() async {
   try {
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    print(googleUser);
 
     final GoogleSignInAuthentication? googleAuth =
         await googleUser?.authentication;
+    print(googleAuth);
 
     final credential = GoogleAuthProvider.credential(
       accessToken: googleAuth?.accessToken,
       idToken: googleAuth?.idToken,
     );
-    
+    print(credential);
+
     final UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
+    print(userCredential.user);
+
+    final user = userCredential.user;
+    if (user != null) {
+      final userDoc = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
+
+      if (!userDoc.exists) {
+        await FirebaseFirestore.instance.collection('users').doc(user.uid).set({
+          'displayName': googleUser?.displayName,
+          'email': user.email,
+          'photoURL': user.photoURL,
+          'uid': user.uid,
+        });
+      }
+    }
 
     return userCredential;
   } on Exception catch (e) {
