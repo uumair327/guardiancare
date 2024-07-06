4c8360f745526d41a5fa8cb4d2028c38f4fee9e1
adarshrawat8304 <adarshrawat8304@gmail.com>
2024-07-06 03:09:10 +0000

    chore: added user name fetch

--- lib/src/features/profile/screens/account.dart
+++ lib/src/features/profile/screens/account.dart
@@ -5,112 +5,143 @@ import 'package:guardiancare/src/features/authentication/screens/loginPage.dart'
 import 'package:guardiancare/src/features/emergency/screens/emergencyContactPage.dart';
 import 'package:guardiancare/src/features/report/screens/reportPage.dart';
 import 'package:guardiancare/src/features/authentication/controllers/login_controller.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Account extends StatelessWidget {
   final User? user;
 
   const Account({super.key, this.user});
 
+  Future<DocumentSnapshot> getUserDetails() async {
+    if (user != null) {
+      return await FirebaseFirestore.instance.collection('users').doc(user!.uid).get();
+    }
+
+    throw Exception("User is null");
+  }
+
   @override
-  Widget build(BuildContext context) {  
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Account'),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CircleAvatar(
-                radius: 50,
-                backgroundImage: user?.photoURL != null
-                    ? NetworkImage(user!.photoURL!)
-                    : null,
-                child:
-                    user?.displayName != null && user!.displayName!.isNotEmpty
-                        ? Text(user!.displayName![0].toUpperCase())
+      body: FutureBuilder<DocumentSnapshot>(
+        future: getUserDetails(),
+        builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text("Error: ${snapshot.error}"));
+          }
+          if (!snapshot.hasData || !snapshot.data!.exists) {
+            return const Center(child: Text("User data not found"));
+          }
+
+          var userData = snapshot.data!.data() as Map<String, dynamic>;
+
+          return SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: userData['photoURL'] != null
+                        ? NetworkImage(userData['photoURL'])
                         : null,
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                'Profile Information',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 10),
-              ListTile(
-                leading: const Icon(Icons.person, color: tPrimaryColor),
-                title: Text('Name: ${user?.displayName ?? 'Not available'}'),
-              ),
-              ListTile(
-                leading: const Icon(Icons.email, color: tPrimaryColor),
-                title: Text('Email: ${user?.email ?? 'Not available'}'),
-              ),
-              const Divider(),
-              const Text(
-                'Child Safety Settings',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              ListTile(
-                leading: const Icon(Icons.phone, color: tPrimaryColor),
-                title: const Text('Emergency Contact'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => EmergencyContactPage(),
+                    child: userData['displayName'] != null && userData['displayName'].isNotEmpty
+                        ? Text(userData['displayName'][0].toUpperCase())
+                        : null,
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Profile Information',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: tPrimaryColor,
                     ),
-                  );
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.warning, color: tPrimaryColor),
-                title: const Text('Report an Incident'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const ReportPage(),
+                  ),
+                  // const SizedBox(height: 5),
+                  ListTile(
+                    leading: const Icon(Icons.person, color: tPrimaryColor),
+                    title: Text('Name: ${userData['displayName'] ?? 'Not available'}'),
+                  ),
+                  ListTile(
+                    minTileHeight: 25,
+                    leading: const Icon(Icons.email, color: tPrimaryColor),
+                    title: Text('Email: ${userData['email'] ?? 'Not available'}'),
+                  ),
+                  const Divider(),
+                  const SizedBox(height: 5),
+                  const Text(
+                    'Child Safety Settings',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: tPrimaryColor,
                     ),
-                  );
-                },
-              ),
-              const Divider(),
-              const Text(
-                'Settings',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              ListTile(
-                leading: const Icon(Icons.logout, color: tPrimaryColor),
-                title: const Text('Log Out'),
-                onTap: () async {
-                  bool result = await signOutFromGoogle();
-                  print(result);
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.phone, color: tPrimaryColor),
+                    title: const Text('Emergency Contact'),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => EmergencyContactPage(),
+                        ),
+                      );
+                    },
+                  ),
+                  ListTile(
+                    minTileHeight: 25,
+                    leading: const Icon(Icons.warning, color: tPrimaryColor),
+                    title: const Text('Report an Incident'),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const ReportPage(),
+                        ),
+                      );
+                    },
+                  ),
+                  const Divider(),
+                  const SizedBox(height: 5),
+                  const Text(
+                    'Settings',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: tPrimaryColor,
+                    ),
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.logout, color: tPrimaryColor),
+                    title: const Text('Log Out'),
+                    onTap: () async {
+                      bool result = await signOutFromGoogle();
+                      print(result);
 
-                  Navigator.pop(
-                    // ignore: use_build_context_synchronously
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const LoginPage(),
-                    ),
-                  );
+                      Navigator.pop(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginPage(),
+                        ),
+                      );
 
-                  if (result) print("Signed Out Successfully !!");
-                },
+                      if (result) print("Signed Out Successfully !!");
+                    },
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
