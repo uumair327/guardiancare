193c02506b1dea83aa53bb44018bced44015a50e
adarshrawat8304 <adarshrawat8304@gmail.com>
2024-08-22 03:42:34 +0000

    initiated v1.1.0 of guardiancare : changes for prod release.

--- lib/src/routing/Pages.dart
+++ lib/src/routing/Pages.dart
@@ -101,11 +101,12 @@ class _PagesState extends State<Pages> {
       extendBody: true,
       appBar: AppBar(
         title: const Text(
-          "Children of India",
+          "Guardian Care",
           style: TextStyle(
             color: tPrimaryColor,
             fontWeight: FontWeight.bold,
             fontSize: 25,
+            fontStyle: FontStyle.italic
           ),
         ),
         centerTitle: true,
