98fc58ad89e90f2295435c986c6437260ac412e3
uumair327 <mohdumair.a@somaiya.edu>
2024-11-03 23:55:26 +0530

    condition outside build

--- lib/src/common_widgets/video_player_page.dart
+++ lib/src/common_widgets/video_player_page.dart
@@ -4,53 +4,63 @@ import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 class VideoPlayerPage extends StatefulWidget {
   final String videoUrl;
 
-  const VideoPlayerPage({super.key, required this.videoUrl});
+  const VideoPlayerPage({Key? key, required this.videoUrl}) : super(key: key);
 
   @override
   State<VideoPlayerPage> createState() => _VideoPlayerPageState();
 }
 
 class _VideoPlayerPageState extends State<VideoPlayerPage> {
+  late YoutubePlayerController _controller;
+  bool _isVideoUrlValid = true;
+
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
+
+    // Extract video ID and check validity
     String? videoId = YoutubePlayer.convertUrlToId(widget.videoUrl);
     if (videoId == null) {
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text('Video Player'),
-        ),
-        body: const Center(
-          child: Text('Invalid YouTube URL'),
+      _isVideoUrlValid = false;
+    } else {
+      _controller = YoutubePlayerController(
+        initialVideoId: videoId,
+        flags: const YoutubePlayerFlags(
+          autoPlay: true,
+          mute: false,
         ),
       );
     }
+  }
 
-    YoutubePlayerController controller = YoutubePlayerController(
-      initialVideoId: videoId,
-      flags: const YoutubePlayerFlags(
-        autoPlay: true,
-        mute: false,
-      ),
-    );
-
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Video Player'),
       ),
       body: Center(
-        child: YoutubePlayer(
-          controller: controller,
-          showVideoProgressIndicator: true,
-          progressIndicatorColor: Colors.blueAccent,
-          progressColors: const ProgressBarColors(
-            playedColor: Colors.blue,
-            handleColor: Colors.blueAccent,
-          ),
-          onReady: () {
-            controller.addListener(() {});
-          },
-        ),
+        child: _isVideoUrlValid
+            ? YoutubePlayer(
+                controller: _controller,
+                showVideoProgressIndicator: true,
+                progressIndicatorColor: Colors.blueAccent,
+                progressColors: const ProgressBarColors(
+                  playedColor: Colors.blue,
+                  handleColor: Colors.blueAccent,
+                ),
+                onReady: () {
+                  _controller.addListener(() {});
+                },
+              )
+            : const Text('Invalid YouTube URL'),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
