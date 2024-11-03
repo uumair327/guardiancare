81f3a466af96f8df78c039e1d5b73bfeaa9ef580
uumair327 <mohdumair.a@somaiya.edu>
2024-11-03 23:32:37 +0530

    services optimization

--- lib/src/api/gemini/process_categories.dart
+++ lib/src/api/gemini/process_categories.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_gemini/flutter_gemini.dart';
-import 'package:guardiancare/src/api/youtube/fetch_videos.dart';
+import 'package:guardiancare/src/api/youtube/controllers/account_controller.dart';
 import 'package:guardiancare/src/constants/keys.dart';
 
 Future<bool> processCategories(
@@ -10,16 +10,17 @@ Future<bool> processCategories(
     final gemini = Gemini.instance;
 
     final response = await gemini.text(
-        "Summarize the subtopics $incorrectCategories under the main topic $category into a single search term for YouTube. The term should effectively encompass all subtopics and the main topic, consisting of 4-5 words, to yield highly relevant and accurate search results. Only provide 2 YouTube search terms, each separated by a new line, and nothing else. Search terms must not be in bullet point format. The search term should be highy relevant with the $incorrectCategories and $category !!");
-
-    // print(response);
-    // print(response?.output);
+      "Summarize the subtopics $incorrectCategories under the main topic $category into a single search term for YouTube. The term should effectively encompass all subtopics and the main topic, consisting of 4-5 words, to yield highly relevant and accurate search results. Only provide 2 YouTube search terms, each separated by a new line, and nothing else. Search terms must not be in bullet point format. The search term should be highly relevant with the $incorrectCategories and $category!",
+    );
 
     if (response != null && response.output != null) {
-      List<String>? searchTerm = response.output!.split('\n');
+      List<String> searchTerms = response.output!.split('\n');
 
-      // Call fetchVideos with search terms
-      await fetchVideos(searchTerm);
+      // Create an instance of AccountController
+      final accountController = AccountController();
+
+      // Call fetchAndSaveVideos with search terms
+      await accountController.fetchAndSaveVideos(searchTerms);
 
       await Future.delayed(const Duration(seconds: 1));
       return true;
