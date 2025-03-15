73ddb2198d1dedd33a2eb7abc10ff8067591b3b6
Adarsh <Adarshbunny293@gmail.com>
2025-03-15 16:08:30 +0530

    Feat : Added Content page for SUFASEC in carousel

--- lib/src/features/home/controllers/home_controller.dart
+++ lib/src/features/home/controllers/home_controller.dart
@@ -1,13 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomeController {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   Future<List<Map<String, dynamic>>> fetchCarouselData() async {
     try {
       QuerySnapshot snapshot =
-          await FirebaseFirestore.instance.collection('carousel_items').get();
-      return snapshot.docs
-          .map((doc) => doc.data() as Map<String, dynamic>)
-          .toList();
+          await _firestore.collection('carousel_items').get();
+
+      return snapshot.docs.map((doc) {
+        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+        return {
+          'type': data['type'] ?? 'image',
+          'imageUrl': data['imageUrl'],
+          'link': data['link'],
+          'thumbnailUrl': data['thumbnailUrl'] ?? '',
+          'content': data['content'] ?? {},
+        };
+      }).toList();
     } catch (e) {
       print('Error fetching carousel data: $e');
       return [];
