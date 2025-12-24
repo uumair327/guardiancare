27183f59a2511e737d422a547dd8e356cb6a16f2
uumair327 <uumair327@gmail.com>
2025-12-25 02:58:53 +0530

    chore: exclude release builds and old_lib from git tracking

--- old_lib/core/constants/api_keys.dart
+++
@@ -1,2 +0,0 @@
-const kGeminiApiKey = "AIzaSyCJz_lIoAxc0ZY1Gk3jBgnLZTKeTbDn6B4";
-const kYoutubeApiKey = "AIzaSyAIuTQTk0_aEawCBLNX-YZwB6qEuFuHnGg";
--- old_lib/features/home/domain/repositories/home_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:guardiancare/core/error/failures.dart';
-import 'package:guardiancare/features/home/domain/entities/carousel_item_entity.dart';
-
-/// Abstract repository interface for home feature
-abstract class HomeRepository {
-  /// Get carousel items as a stream for real-time updates
-  Stream<Either<Failure, List<CarouselItemEntity>>> getCarouselItems();
-}
